f9d4ed86cee5673467c5a97411232e13c12cf856
jrfs22 <mr.jrfs22@gmail.com>
2023-05-10 09:32:13 +0700

    Ini adalah commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+</script>
+
+<template>
+  <div>
+    <h1>IDShop</h1>
+    <p class="animate_animated animate_fadeInRight">
+      Lorem
+    </p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
